913f5a90827324828cf3a4dbee271a248cb173c9
nikhilkc96 <nikhilkc96@gmail.com>
2021-03-30 15:37:28 -0400

    repartition

--- G26HW1.py
+++ G26HW1.py
@@ -23,28 +23,24 @@ def main():
     K =sys.argv[1]
     assert K.isdigit(), "K Should be an int"
     K = int(K)
-    print(K)
     #Read The T from the argv
     T =sys.argv[2]
     assert T.isdigit(), "T Should be an int"
     T = int(T)
-    print(T)
     #Read the file path from the argv
     data_path = sys.argv[3]
     assert os.path.isfile(data_path), "File or folder not found"
-    RawData = spark.read.csv(data_path).cache()
-    RawData.repartition(numPartitions=K)
+    RawData = spark.read.csv(data_path)
+    #Partition of the data given 
+    RawData =  RawData.repartition(K)
     RawData.show()
+    print("K::" +str(K) +" \nT::"+str(T))
+    print("Number of Partitions ::",RawData.rdd.getNumPartitions())
+    print("Number of Samples :: ", RawData.count())
 
-
-
-
-    #normalizedRatings =
-
-
-    # SETTING GLOBAL VARIABLES
-    numsam = RawData.count()
-    print("Number of Samples :: ", numsam)
+    # RDD String (RawData) to RDD Pair(normalizedRatings)
+    normalizedRatings = RawData.map()
+                        
 
 
 if __name__ == "__main__":
