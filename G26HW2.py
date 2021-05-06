52f290e02dca6c83673b59743dfb2f4a4ba79825
Nikhil K C <nikhilkc96@gmail.com>
2021-05-06 22:59:33 +0200

    .

--- G26HW2.py
+++ G26HW2.py
@@ -1,14 +1,35 @@
 import os
 import sys
 from pyspark import SparkContext, SparkConf
+from pyspark.ml.clustering import KMeans
+import math
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml.feature import VectorAssembler
 
+
+
+
+def strToTuple(line):
+    ch = line.strip().split(",")
+    point = tuple(float(ch[i]) for i in range(len(ch)-1))
+    return (point, int(ch[-1]))  # returns (point, cluster_index)
+
+
+def geo_dist(lat1, lon1,lat2,lon2):
+    R = 6372.8
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    dphi = math.radians(lat2 - lat1)
+    dlambda = math.radians(lon2 -lon1)
+    a = math.sin(dphi/2)**2 + math.cos(phi1) * math.cos(phi2)*math.sin(dlambda/2)**2
+    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
 def main():
     #check for ars in the cmd
-    assert len(sys.argv) == 4, "python3 G26HW2.py <K> <T> <csvFileName>"
+    assert len(sys.argv) == 4, "python3 G26HW2.py <K> <csvFileName>"
 
     #Spark Setup
-    conf = SparkConf().setAppName('G26HW1').setMaster("local[*]")
+    conf = SparkConf().setAppName('G26HW2').setMaster("local[*]")
     sc = SparkContext(conf=conf)
     #Read inputs
     #Read k from the argv
@@ -22,17 +43,21 @@ def main():
     #Read the file path from the argv
     data_path = sys.argv[3]
     assert os.path.isfile(data_path), "File or folder not found"
-    RawData = sc.textFile(data_path)
+    fullClustering = sc.textFile(data_path).map(strToTuple)
     #Partition of the data given
-    RawData = RawData.repartition(numPartitions=K)
-    print("Number of Partitions ::", RawData.getNumPartitions())
-    print("Number of Samples :: ", RawData.count())
+    fullClustering = fullClustering.repartition(numPartitions=K)
+    print("Number of Partitions ::", fullClustering.getNumPartitions())
+    print("Number of Samples :: ", fullClustering.count())
     print("INPUT PARAMETERS: K="+str(K)+", T=" +
           str(T)+", "+"file="+str(data_path)+"\n")
     print('OUTPUT:')
+    
+
+    
 
    
 
 
 if __name__ == "__main__":
-	main()
+    main()
+
