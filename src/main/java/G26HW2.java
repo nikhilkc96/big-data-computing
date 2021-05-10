38d69c309d60dd4eb2860e6bb2a5ac6f69a5b8d4
Nikhil K C <nikhilkc96@gmail.com>
2021-05-10 23:01:18 +0200

    Revert "bug fix"
    This reverts commit 8265698122b1eb4b0b1153f73aa3a3e2a4bd9f54.

--- src/main/java/G26HW2.java
+++ src/main/java/G26HW2.java
@@ -10,26 +10,27 @@ import scala.Tuple2;
 import java.io.IOException;
 import java.util.*;
 
-public class G26HW2 {
-    public static Tuple2<Vector, Integer> strToTuple(String str) {
+public class G26HW2  {
+    public static Tuple2<Vector, Integer> strToTuple (String str){
         String[] tokens = str.split(",");
         double[] data = new double[tokens.length];
-        for (int i = 0; i < tokens.length - 1; i++) {
+        for (int i = 0; i < tokens.length-1; i++) {
             data[i] = Double.parseDouble(tokens[i]);
         }
         Vector point = Vectors.dense(data);
-        Integer cluster = Integer.valueOf(tokens[tokens.length - 1]);
+        Integer cluster = Integer.valueOf(tokens[tokens.length-1]);
         Tuple2<Vector, Integer> pair = new Tuple2<>(point, cluster);
         return pair;
     }
-
     public static void main(String[] args) throws IOException {
+
         //Sets Spark configuration
         SparkConf conf = new SparkConf(true).setAppName("G26HW2");
         JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("WARN");
+
         //Check the args count
-        if (args.length != 3) {
+        if(args.length != 3){
             throw new IllegalArgumentException("Usage: Check the Args:: <CSV_File> <K> <T>");
         }
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
@@ -37,11 +38,11 @@ public class G26HW2 {
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         //Read the args array
         String file = args[0];
-        int K = Integer.parseInt(args[1]);
+        int K =  Integer.parseInt(args[1]);
         int T = Integer.parseInt(args[2]);
         Random rand = new Random();
         long size = sc.textFile(file).cache().count();
-        JavaPairRDD<Vector, Integer> fullClustering = sc.textFile(file).mapToPair(x -> strToTuple(x)).repartition(8).cache();
+        JavaPairRDD<Vector,Integer> fullClustering = sc.textFile(file).mapToPair(x -> strToTuple(x)).repartition(8).cache();
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         //                PART 2
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
@@ -50,76 +51,70 @@ public class G26HW2 {
         //                PART 3
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         Broadcast<List<Tuple2<Vector, Integer>>> clusteringSample = sc.broadcast(fullClustering.filter((x) -> {
-            return rand.nextDouble() <= Math.min(T / sharedClusterSizes.getValue().get(x._2).doubleValue(), 1);
-        }).collect());
+            return rand.nextDouble() <= Math.min(T/sharedClusterSizes.getValue().get(x._2).doubleValue(), 1); }).collect());
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         //                PART 4
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        long time_approx_start = System.currentTimeMillis();
-        JavaRDD<Double> Score = fullClustering.map((x) -> {
-            double sum = 0d;
-            double[] par = new double[K];
+        long time_approx_start =System.currentTimeMillis();
+        JavaRDD<Double> Score =fullClustering.map((x) ->{
+            double[] sum = new double[K];
+            double[] par= new double[K];
             Vector ClusterPoint = x._1;
-            int ClusterIndex = x._2;
-            for (Tuple2<Vector, Integer> tuple2 : clusteringSample.getValue()) {
+            int ClusterIndex =x._2;
+            for( Tuple2<Vector, Integer> tuple2 : clusteringSample.getValue()){
                 Vector ClusterPoint2 = tuple2._1;
-                int ClusterIndex2 = tuple2._2;
-                if (ClusterIndex == ClusterIndex2) {
-                    sum += Vectors.sqdist(ClusterPoint, ClusterPoint2);
-                }
-                if (ClusterIndex != ClusterIndex2) {
-                    par[ClusterIndex2] += Vectors.sqdist(ClusterPoint, ClusterPoint2);
+                int ClusterIndex2 =tuple2._2;
+                sum[ClusterIndex2] += Vectors.sqdist(ClusterPoint,ClusterPoint2);
 
+            }
+            for(int i=0;i<K;i++) {
+                par[i] = Math.min(T/sharedClusterSizes.getValue().get(x._2).doubleValue(), 1);
+                for(int j=0;j<K;j++){
+                    sum[j] *= par[j];
                 }
             }
-            double approx1 = sum / Math.min(T, sharedClusterSizes.getValue().get(ClusterIndex).doubleValue());
+            double approx1 = sum[ClusterIndex];
             double approx2 = Double.MAX_VALUE;
-            for (int i = 0; i < K; i++) {
-                if (i != ClusterIndex) {
+            for (int i=0;i<K;i++){
+                if(i !=ClusterIndex) {
                     approx2 = Math.min(par[i] / Math.min(T, sharedClusterSizes.getValue().get(i).doubleValue()), approx2);
                 }
             }
-            return (approx2 - approx1) / Math.max(approx1, approx2) / size;
+            return (approx2-approx1)/Math.max(approx1,approx2)/size;
         });
-        double approxSilhFull = Score.reduce((x, y) -> x + y);
-        long time_approx_end = System.currentTimeMillis();
+        double approxSilhFull =Score.reduce((x,y) -> x+y);
+        long time_approx_end =System.currentTimeMillis();
 
-        double[] SamclustersSize = new double[K];
-        for (Tuple2<Vector, Integer> tuple : clusteringSample.getValue()) {
-            int clusterIndex = tuple._2;
-            SamclustersSize[clusterIndex] = SamclustersSize[clusterIndex] + 1;
-        }
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         //                PART 5
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        long time_exact_start = System.currentTimeMillis();
-        double exactSilhSample = 0d;
-        for (Tuple2<Vector, Integer> tuple1 : clusteringSample.getValue()) {
+
+
+        long time_exact_start =System.currentTimeMillis();
+        int[] SamclustersSize = new int[K];
+        double exactSilhSample = 0;
+        for (Tuple2<Vector, Integer> tuple : clusteringSample.getValue()) {
+            int clusterIndex = tuple._2;
+            SamclustersSize[clusterIndex] = SamclustersSize[clusterIndex] + 1;
+        }
+        for( Tuple2<Vector, Integer> tuple1 : clusteringSample.getValue()) {
             Vector ClusterPoint = tuple1._1;
             int ClusterIndex = tuple1._2;
-            double sum = 0d;
-            double par[] = new double[K];
+            double[] sum = new double[K];
             for (Tuple2<Vector, Integer> tuple2 : clusteringSample.getValue()) {
                 Vector ClusterPoint2 = tuple2._1;
                 int ClusterIndex2 = tuple2._2;
-                if (!ClusterPoint.equals(ClusterPoint2) && ClusterIndex == ClusterIndex2) {
-                    sum += Vectors.sqdist(ClusterPoint, ClusterPoint2);
-                }
-                if (ClusterIndex != ClusterIndex2) {
-                    par[ClusterIndex2] += Vectors.sqdist(ClusterPoint, ClusterPoint2);
-
+                sum[ClusterIndex2] += Vectors.sqdist(ClusterPoint, ClusterPoint2);
+            }
+            double approx1 = SamclustersSize[ClusterIndex];
+            double approx2 = Double.MAX_VALUE;
+            for (int i = 0; i < K; i++) {
+                if (i != ClusterIndex) {
+                    approx2 = Math.min(SamclustersSize[i] / Math.min(T, sharedClusterSizes.getValue().get(i).doubleValue()), approx2);
                 }
             }
-                double approx1 = sum / SamclustersSize[ClusterIndex];
-                double approx2 = Double.MAX_VALUE;
-                for (int i = 0; i < K; i++) {
-                    if (i != ClusterIndex) {
-                        approx2 = Math.min(par[i] / SamclustersSize[i],approx2);
-                }}
-
-                double SilhSample = (approx2 - approx1 )/Math.max(approx1, approx2) / clusteringSample.getValue().size();
-            exactSilhSample +=SilhSample;
+            exactSilhSample = approx2 - approx1 / Math.max(approx1, approx2) / clusteringSample.getValue().size();
         }
             long time_exact_end = System.currentTimeMillis();
             // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
